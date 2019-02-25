b0266e6208251927a0fee90125317d5af0d8ef9d
syw2014 <jerryshi0110@gmail.com>
2019-02-25 13:44:04 +0800

    fix version bugs, change py2 version to py3

---
+++ projects/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author  : Jerry.Shi
+# File    : __init__.py.py
+# PythonVersion: python3.5
+# Date    : 2019/2/22 17:14
+# Software: PyCharm
--- projects/icme2019_baseline/train.sh
+++ projects/icme2019_baseline/train.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 cd  `dirname $0`
+
 echo `pwd`
-training_path=$1
-validation_path=$2
+
+DATA_DIR = "/data/research/data/short_video/icme2019/dataset/"
+
+training_path="$DATA_DIR/final_track2_train.txt.80"     # 交互文件,final_track[1-2]_train.txt
+validation_path="$DATA_DIR/final_track2_valid.txt.20"   # 需要自行划分验证集
+
 echo "training path: " $training_path
 echo "validation path: " $validation_path
 
-save_model_dir=$3
+
+save_model_dir="./output"     # output model directory
 echo "save model on: " $save_model_dir
 
-batch_size=$4
-embedding_size=$5
+# parameters for model
+# baseline for track2 like task: embedding_size=40, optimizer=adam, lr=0.0005, auc=0.865
+# baseline for finish task: embedding_size=40, optimizer=adam, lr=0.0001, auc=0.698
+
+batch_size=32
+embedding_size=40
 echo "batch size: " $batch_size
 echo "embedding size: " $embedding_size
 
-optimizer=$6
-lr=$7
+optimizer="adam"
+lr=0.0005
 
-task=$8
-track=$9
+task="finish" # finish or like
+track=2     # track1 or track2
 echo "task: " $task
 echo "track: " $track
 
