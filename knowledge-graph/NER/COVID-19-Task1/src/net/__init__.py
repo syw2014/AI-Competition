8a8048820f595b7fa1e3a8c04267b323d85e6063
syw2014 <jerryshi0110@gmail.com>
2020-06-16 13:46:44 +0800

    organized folder, utils to store common functions, net to store usefull layers

---
+++ knowledge-graph/NER/COVID-19-Task1/src/net/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author  : Jerry.Shi
+# File    : __init__.py.py
+# PythonVersion: python3.6
+# Date    : 2020/6/16 11:55
+# Software: PyCharm
+"""Define normal layers for deep network, like various losses, metrics."""
