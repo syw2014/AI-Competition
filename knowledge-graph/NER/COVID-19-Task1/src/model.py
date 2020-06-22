a7c0d64424fde48dda93bddb659a5fc607a93325
syw2014 <jerryshi0110@gmail.com>
2020-06-22 10:22:54 +0800

    start designed BiLSTM-CRF model with tf2.0 and tensorflow-addons(CRF)

---
+++ knowledge-graph/NER/COVID-19-Task1/src/model.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author  : Jerry.Shi
+# File    : model.py
+# PythonVersion: python3.6
+# Date    : 2020/6/22 8:49
+# Software: PyCharm
+"""Create a Bi-LSTM+CRF as baseline for NER."""
+
+import tensorflow as tf
+import tensorflow_addons as tfa
+import numpy as np
+
+
+class BiLSTMCRF(tf.keras.Model):
+    def __init__(self, hidden_num, vocab_size, label_size, embedding_dim,
+                 max_seq_len,
+                 weights=None,
+                 weights_trainable=False):
+        super(BiLSTMCRF, self).__init__()
+        self.hidden_num = hidden_num
+        self.vocab_size = vocab_size
+        self.label_size = label_size
+        self.embedding_dim = embedding_dim
+        self.max_seq_len = max_seq_len
+
+        # define layers
+        if weights is None:
+            weights = np.array(weights)     # use pre-trained w2v
+            self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                                                       weights=[weights],
+                                                       trainable=weights_trainable)
+        else:
+            self.embedding = tf.keras.layers.Embedding(vocab_size,
+                                                       embedding_dim,
+                                                       input_length=max_seq_len)
+        self.lstm_cell = tf.keras.layers.LSTM(hidden_num, return_sequences=True)
+        self.biLSTM = tf.keras.layers.Bidirectional(self.lstm_cell, merge_mode='concat')
+        self.dense = tf.keras.layers.Dense(label_size)
+
+        # define transition
+        self.transition_params = tf.Variable(tf.random_uniform(shape=(label_size, label_size)))
+
+        self.dropout = tf.keras.layers.Dropout(0.3)
+
+    @tf.function
+    def call(self, inputs, labels, training=None):
+        embedding = self.embedding(inputs)
+        dropout = self.dropout(embedding, training)
+        encoding = self.biLSTM(dropout)
+        output = self.dense(encoding)
+
+        log_likelihood, transition_params = tfa.text.crf_log_likelihood(output,
+                                                                        labels,
+                                                                        self.max_seq_len)
+
+        return output, log_likelihood
+
+    def predict(self, inputs, training=None):
+        embedding = self.embedding(inputs)
+        dropout = self.dropout(embedding, training)
+        encoding = self.biLSTM(dropout)
+        output = self.dense(encoding)
+
+        return output
